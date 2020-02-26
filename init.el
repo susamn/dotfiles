2129fd294a6142bbce5f04911da9725f5d8368b4
ssaman002c <supratim_samanta@cable.comcast.net>
2020-02-26 14:06:01 -0500

    Moving to Org based configuration

--- init.el
+++ init.el
@@ -1,236 +1,36 @@
-(setq user-full-name "Supratim Samanta"
-      user-mail-address "supratim.iee23.ju2005@gmail.com")
-(setq gc-cons-thresold 50000000)
-(setq large-file-warning-thresold 100000000)
-
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-
+;; Configure package.el to include MELPA.
 (require 'package)
-(setq package-enable-at-startup nil)
-1(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
+
+;; Ensure that use-package is installed.
+;;
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+(when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile (require 'use-package))
-
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-(blink-cursor-mode -1)
-
-(global-hl-line-mode +1)
-(line-number-mode +1)
-(global-display-line-numbers-mode +1)
-(column-number-mode t)
-(size-indication-mode t)
-
-(setq inhibit-startup-screen t)
-
-(setq frame-title-format
-      '((:eval (if (buffer-file-name)
-       (abbreviate-file-name (buffer-file-name))
-       "%b"))))
-
-(setq scroll-margin 0
-      scroll-conservatively 100000
-      scroll-preserve-screen-position 1)
-(set-frame-font "Hack 12" nil t)
-
-(use-package doom-themes
-  :ensure t
-  :config
-  (load-theme 'doom-one t)
-  (doom-themes-visual-bell-config))
+(org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (doom-dark+)))
  '(custom-safe-themes
    (quote
-    ("3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default)))
+    ("a339f231e63aab2a17740e5b3965469e8c0b85eccdfb1f9dbd58a30bdad8562b" "a83f05e5e2f2538376ea2bfdf9e3cd8b7f7593b16299238c1134c1529503fa88" default)))
  '(package-selected-packages
    (quote
-    (yasnippet-classic-snippets magit orgit golden-ratio ccls company-lsp lsp-ui lsp-mode helm-projectile company helm projectile flycheck avy which-key crux expand-region smartparens diminish smart-mode-line doom-themes use-package))))
+    (yasnippet-snippets yasnippet evil-org evil-surround evil-magit evil-collection evil crux expand-region smartparens helm-projectile helm projectile use-package org-bullets magit git-timemachine flycheck doom-themes diff-hl company auto-compile airline-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(use-package smart-mode-line-powerline-theme
-  :ensure t)
-
-(use-package smart-mode-line
-  :ensure t
-  :config
-  (setq sml/theme 'powerline)
-  (add-hook 'after-init-hook 'sml/setup))
-
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-(fset 'yes-or-no-p 'y-or-n-p)
-
-(global-auto-revert-mode t)
-
-(setq-default tab-width 4
-              indent-tabs-mode nil)
-
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
-
-(add-hook 'before-save-hook 'whitespace-cleanup)
-
-(use-package diminish
-  :ensure t)
-
-(use-package smartparens
-  :ensure t
-  :diminish smartparens-mode
-  :config
-  (progn
-    (require 'smartparens-config)
-    (smartparens-global-mode 1)
-    (show-paren-mode t)))
-
-;; Purpose : Selecting a region becomes smarter with expand region which keeps selecting an increasing region based on dwim syntax.
-
-(use-package expand-region
-  :ensure t
-  :bind ("M-m" . er/expand-region))
-
-;; Some useful defaults are provided by the crux package of Prelude fame. “C-k” now kills a line if nothing is selected. “C-a” now toggles between first letter on the line, or beginning of the line.
-(use-package crux
-  :ensure t
-  :bind
-  ("C-k" . crux-smart-kill-line)
-  ("C-c n" . crux-cleanup-buffer-or-region)
-  ("C-c f" . crux-recentf-find-file)
-  ("C-a" . crux-move-beginning-of-line))
-
-;;Given the thousands of commands at your disposal, its only reasonable to have a package help you figure out the next keystroke. which-key package opens a small buffer at the bottom with suggestions for next keystroke and possible commands that are available.
-(use-package which-key
-  :ensure t
-  :diminish which-key-mode
-  :config
-  (which-key-mode +1))
-
-;; I can jump around in the current visual field using avy’s go to char.
-(use-package avy
-  :ensure t
-  :bind
-  ("C-=" . avy-goto-char)
-  :config
-  (setq avy-background t))
-
-;; Purpose : I use company mode to provide cmpletion and flycheck to do syntax checking and enable them globally.
-(use-package company
-  :ensure t
-  :diminish company-mode
-  :config
-  (add-hook 'after-init-hook #'global-company-mode))
-
-(use-package flycheck
-  :ensure t
-  :diminish flycheck-mode
-  :config
-  (add-hook 'after-init-hook #'global-flycheck-mode))
-
-;; Purpose : No project is begun without git and Emacs community’s integration with git through magit is unparalleled in its expanse and ease of use. Setting that up is just as easy. “C-M-g” now triggers a git status buffer for the current file’s repository.
-
-(use-package magit
-  :bind (("C-M-g" . magit-status)))
-
-
-;; Purpose : Projectile is a project manager that lets you easily switch between files in a project and seamlessly between projects as well. I use it with helm which I set up below.
-(use-package projectile
-  :ensure t
-  :diminish projectile-mode
-  :bind
-  (("C-c p f" . helm-projectile-find-file)
-   ("C-c p p" . helm-projectile-switch-project)
-   ("C-c p s" . projectile-save-project-buffers))
-  :config
-  (projectile-mode +1)
-)
-
-;; Purpose : HELM : It is a dwim fuzzy completion framework for Emacs and makes navigating Emacs a much nicer experience overall. I like to setup Helm to be a comfortable 20 pts in height and bind the most frequent Emacs commands like “M-x” with the helm equivalents.
-(use-package helm
-  :ensure t
-  :defer 2
-  :bind
-  ("M-x" . helm-M-x)
-  ("C-x C-f" . helm-find-files)
-  ("M-y" . helm-show-kill-ring)
-  ("C-x b" . helm-mini)
-  ("C-s" . helm-occur-from-isearch)
-  :config
-  (require 'helm-config)
-  (helm-mode 1)
-  (setq helm-split-window-inside-p t
-    helm-move-to-line-cycle-in-source t)
-  (setq helm-autoresize-max-height 0)
-  (setq helm-autoresize-min-height 20)
-  (helm-autoresize-mode 1)
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-  )
-
-;;Purpose : I combine it with projectile to show project files through a helm fuzzy find interface.
-(use-package helm-projectile
-  :ensure t
-  :config
-  (helm-projectile-on))
-
-;; Some other packages
-(use-package yasnippet
-  :ensure t
-  :config (use-package yasnippet-snippets :ensure t) (yas-reload-all))
-(yas-global-mode 1)
-
-(use-package lsp-mode :commands lsp :ensure t)
-(use-package lsp-ui :commands lsp-ui-mode :ensure t)
-(use-package company-lsp
-  :ensure t
-  :commands company-lsp
-  :config (push 'company-lsp company-backends))
-
-(require 'switch-window)
-(global-set-key (kbd "C-x o") 'switch-window)
-(global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
-(global-set-key (kbd "C-x 2") 'switch-window-then-split-below)
-(global-set-key (kbd "C-x 3") 'switch-window-then-split-right)
-(global-set-key (kbd "C-x 0") 'switch-window-then-delete)
-
-(global-set-key (kbd "C-x 4 d") 'switch-window-then-dired)
-(global-set-key (kbd "C-x 4 f") 'switch-window-then-find-file)
-(global-set-key (kbd "C-x 4 m") 'switch-window-then-compose-mail)
-(global-set-key (kbd "C-x 4 r") 'switch-window-then-find-file-read-only)
-
-(global-set-key (kbd "C-x 4 C-f") 'switch-window-then-find-file)
-(global-set-key (kbd "C-x 4 C-o") 'switch-window-then-display-buffer)
-
-(global-set-key (kbd "C-x 4 0") 'switch-window-then-kill-buffer)
-
-(require 'org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-(setq org-hide-leading-stars t)
-
-(require 'powerline)
-(require 'airline-themes)
-(load-theme 'airline-dark)
-
-;; Purpose: Daemon mode
-(require 'server)
-(if (not (server-running-p)) (server-start))

635593d2fd148211503601692047cba3a5efc287
susamn <supratim.iee23.ju2005@gmail.com>
2025-03-25 23:20:45 -0400

    Adding nvim lspconfig and initial setup for some languages

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,8 +9,22 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls"}
+        ensure_installed = {"lua_ls","jdtls","gopls","pylsp"}
       })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.jdtls.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.pylsp.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }

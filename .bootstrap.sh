c8d42fb7fc5499f7bf7ef3af30bb6bb5a91193cf
susamn <supratim.iee23.ju2005@gmail.com>
2025-03-29 11:06:41 -0400

    Creating folder once the /home/susamn/workspace is set

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,5 +1,5 @@
-mkdir -p ~/workspace/sdk ~/workspace/scripts ~/workspace/install 
 export WORKSPACE_PATH=~/workspace
+mkdir -p $WORKSPACE_PATH/sdk $WORKSPACE_PATH/scripts $WORKSPACE_PATH/install 
 source $WORKSPACE_PATH/.variables.sh
 source $WORKSPACE_PATH/.aliases.sh
 #source ~/workspace/.variables.sh

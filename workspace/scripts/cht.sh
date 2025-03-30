cdf6ec292470591c07883954ae06b4c72ba409a9
Supratim Samanta <supratim.iee23.ju2005@gmail.com>
2025-03-30 07:33:12 -0400

    Changing the cht.sh script to use /home/susamn/workspace/scripts

--- workspace/scripts/cht.sh
+++ workspace/scripts/cht.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-selected=`cat ~/workspace/scripts/.langs ~/workspace/scripts/.commands | fzf`
+selected=`cat $SCRIPTS_PATH/.langs $SCRIPTS_PATH/.commands | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/workspace/scripts/.langs; then
+if grep -qs "$selected" $SCRIPTS_PATH/.langs; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else

85459d4ee457c18bd0d7b3ca21a9d31d62613ff2
Supratim Samanta <supratim.iee23.ju2005@gmail.com>
2025-10-08 17:23:51 -0400

    Makign docker ps lines of uniform length

--- .config/conky/get-docker-info.sh
+++ .config/conky/get-docker-info.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 docker ps --format "{{.ID}}|{{.Names}}|{{.Ports}}|{{.Status}}" | awk -F'|' '{
     id=substr($1,1,6)
-    name=substr($2,1,15)
+    name=substr($2,1,12)
     ports=$3
     status=$4
     gsub(/0.0.0.0:/, "", ports)
     gsub(/->.*/,"",ports)
+    gsub(/ /,"",ports)
     gsub(/Up /,"",status)
     gsub(/ \(.*\)/,"",status)
     gsub(/ seconds?/,"s",status)
@@ -14,5 +15,11 @@ docker ps --format "{{.ID}}|{{.Names}}|{{.Ports}}|{{.Status}}" | awk -F'|' '{
     gsub(/ days?/,"d",status)
     gsub(/ weeks?/,"w",status)
     if(ports=="") ports="-"
-    printf "%s | %s | %s | %s\n", id, name, substr(ports,1,10), substr(status,1,8)
+
+    # Truncate ports if too long, add ellipsis
+    if(length(ports) > 10) {
+        ports = substr(ports,1,7) "..."
+    }
+
+    printf "%s|%-12s|%-10s|%s\n", id, name, ports, substr(status,1,6)
 }' | head -5
